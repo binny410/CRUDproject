5dd2b4b473709a68b91da0acb14117c52b373d4e
unknown <balpreetkaur410@gmail.com>
2021-01-27 13:36:01 -0500

    version 1.1

--- js/main.js
+++ js/main.js
@@ -10,59 +10,68 @@ retrieveData = JSON.parse(fetchData);
 //Function to upload data after clicking submit button.
 function submitData() {
     //Edit function will work
-    if ($("#submitEdit").hasClass('editing')) {
+    if ($("#submit").hasClass('editing')) {
         //getting the index of editing line.
-        var z = $("#submitEdit").attr('indexVal');
-        console.log(z);
-        //getting values from local storage.
-        retrieveData[z].Name = $("#Mname").val();
-        retrieveData[z].Email = $("#Memail").val();
-        retrieveData[z].Address = $("#Maddress").val();
-        retrieveData[z].Copy = $("input:radio[name=Mctype]:checked").val();
-        retrieveData[z].Message = $("#Mmessage").val();
-        localStorage.setItem('userData', JSON.stringify(retrieveData))
-        location.reload();
+        var z = $("#submit").attr('indexVal');
+        //        console.log(z);
+        if (($("#Mname").val() && $("#Memail").val() && $("#Maddress").val() && $("#Mmessage").val()) != '') {
+            //getting values from local storage.
+            retrieveData[z].Name = $("#Mname").val();
+            retrieveData[z].Email = $("#Memail").val();
+            retrieveData[z].Address = $("#Maddress").val();
+            retrieveData[z].Copy = $("input:radio[name=Mctype]:checked").val();
+            retrieveData[z].Message = $("#Mmessage").val();
+
+            localStorage.setItem('userData', JSON.stringify(retrieveData))
+            location.reload();
+        } else {
+            alert("Complete all the fields.")
+        }
     }
     //Regular submit function will run.
     else {
-
         var inputName = $("#name").val();
         var inputEmail = $("#email").val();
         var inputAddress = $("#address").val();
         var inputCopy = $("input:radio[name=ctype]:checked").val()
         var inputMessage = $("#message").val();
 
-        //Creating object with user data.
-        var userData = {
-            "Name": inputName,
-            "Email": inputEmail,
-            "Address": inputAddress,
-            "Copy": inputCopy,
-            "Message": inputMessage
+        if ((inputName && inputEmail && inputAddress && inputMessage) != "") {
+            //Creating object with user data.
+            var userData = {
+                "Name": inputName,
+                "Email": inputEmail,
+                "Address": inputAddress,
+                "Copy": inputCopy,
+                "Message": inputMessage
+            }
+
+            //If there is no data stored in local storage  then retrieveData is empty array.
+            if (retrieveData == null) retrieveData = []
+
+            //Adding fresh data to the retrieved data from local storage.
+            retrieveData.push(userData);
+
+            //Stringifying all data to JSON format.
+            var jsonData = JSON.stringify(retrieveData);
+
+            //Adding the data to local storage.
+            localStorage.setItem("userData", jsonData);
+
+            //Displaying the entered data to the table.
+            $("tbody").append("<tr> <td> " + inputName + " </td> <td> " + inputEmail + " </td> <td> " + inputAddress + " </td><td> " + inputCopy + " </td> <td> " + inputMessage + "</td><td> <button class='delete btn btn-secondary'  onclick='deleteData(this);'>DELETE</button> <button class='edit btn btn-light' data-toggle='modal' data-target='#myModal' onclick='editData(this);'>EDIT</button></td></tr>")
+
+            //Setting input fields to empty again.
+            $("#name").val("");
+            $("#email").val("");
+            $("#address").val("");
+            $("#message").val("");
+
+        } else {
+            alert("Please complete all the fields")
         }
-
-        //If there is no data stored in local storage  then retrieveData is empty array.
-        if (retrieveData == null) retrieveData = []
-
-        //Adding fresh data to the retrieved data from local storage.
-        retrieveData.push(userData);
-
-        //Stringifying all data to JSON format.
-        var jsonData = JSON.stringify(retrieveData);
-
-        //Adding the data to local storage.
-        localStorage.setItem("userData", jsonData);
-
-        //Displaying the entered data to the table.
-        $("tbody").append("<tr> <td> " + inputName + " </td> <td> " + inputEmail + " </td> <td> " + inputAddress + " </td><td> " + inputCopy + " </td> <td> " + inputMessage + "</td><td> <button class='delete btn btn-secondary'  onclick='deleteData(this);'>DELETE</button> <button class='edit btn btn-light' data-toggle='modal' data-target='#myModal' onclick='editData(this);'>EDIT</button></td></tr>")
-
-        //Setting input fields to empty again.
-        $("#name").val("");
-        $("#email").val("");
-        $("#address").val("");
-        $("#message").val("");
-
     }
+
 }
 //Function to retrieve data.
 function getData() {
@@ -96,7 +105,7 @@ function deleteData(e) {
 function editData(e) {
 
     //add a class to the submit button in the form when clicked on the edit button.
-    $("#submitEdit").addClass('editing');
+    $("#submit").addClass('editing');
     //getting index of the row for editing.
     var y = ($(e).parents('tr').index())
     console.log("index of clicked button: " + y);
